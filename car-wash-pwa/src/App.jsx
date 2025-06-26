69edbd9fcddf7b2ef1536e05fa6706d6e4dbccf1
Abner Carvalho <abnercarvalho@me.com>
2025-06-26 22:42:21 +0100

    Initial commit

---
+++ car-wash-pwa/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '2rem', maxWidth: '500px', margin: '0 auto' }}>
+      <h1>Car Wash Booking</h1>
+      <p>Let's get your car clean!</p>
+    </div>
+  );
+}
+
+export default App;
